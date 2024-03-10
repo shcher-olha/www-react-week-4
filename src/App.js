fe84df3b88d78c66d26a2439ec132b63977cf024
shcher-olha <shcher.olha@gmail.com>
2024-03-10 20:01:41 +0100

    add links gitHub

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather city="Vaduz" />
-        
+
         <footer>
           This project is created by {""}
-          <a href="#" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/shcher-olha"
+            target="_blank"
+            rel="noreferrer"
+          >
             Shcherbak Olha
           </a>
           and is {""}
-          <a href="#" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/shcher-olha/www-react-week-4"
+            target="_blank"
+            rel="noreferrer"
+          >
             open sourced on GitHub
           </a>
         </footer>
